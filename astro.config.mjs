7e61f09f8b2853950cbb862657f21f86b779fb89
Carl-David Granbäck <cgranbac@volvocars.com>
2024-06-20 13:24:20 +0200

    Re-add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    compress({
+      SVG: false,
+    }),
+  ],
   build: {
     inlineStylesheets: 'always',
   },
